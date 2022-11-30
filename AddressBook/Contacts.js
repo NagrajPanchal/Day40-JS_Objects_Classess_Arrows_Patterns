20c1c89f49d5a024ac01e997fdf08d3ad2ed78a7
NagrajPanchal <nagrajsubhashpanchal@gmail.com>
2022-11-30 23:07:38 +0530

    Added : Address Book and Objects Classses Arrows Functions Patterns

---
+++ AddressBook/Contacts.js
@@ -0,0 +1,16 @@
+class Contacts
+{
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email)
+    {
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.address=address;
+        this.city=city;
+        this.state=state;
+        this.zip=zip;
+        this.phoneNumber=phoneNumber;
+        this.email=email;
+    }
+}
+let nagrajPanchal = new Contacts('Nagraj','Panchal','Shivanagar','Aurad','Karnataka','585326','9739713842','nagraj.panchal@gmail.com');
+console.log(nagrajPanchal);
