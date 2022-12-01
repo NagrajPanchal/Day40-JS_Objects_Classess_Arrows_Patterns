919566b96ebe386b8e2fe787f709b14a4eb04a7b
NagrajPanchal <nagrajsubhashpanchal@gmail.com>
2022-12-01 10:26:14 +0530

    Added : Address Book validation of Contacts

--- AddressBook/Contacts.js
+++ AddressBook/Contacts.js
@@ -1,17 +1,26 @@
-// UC 1 Create Address Book Contacts Details
+// UC 2 Validate Contacts
 class Contacts
 {
     constructor(firstName, lastName, address, city, state, zip, phoneNumber, email)
     {
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.address=address;
-        this.city=city;
-        this.state=state;
-        this.zip=zip;
-        this.phoneNumber=phoneNumber;
-        this.email=email;
+        this.firstName=/[A-Z]{1}[a-z]{2,}/.test(firstName);
+        this.lastName=/[A-Z]{1}[a-z]{2,}/.test(lastName);
+        this.address=/[a-zA-Z0-9\s]{3,}/.test(address);
+        this.city=/[a-zA-Z0-9\s]{3,}/.test(city);
+        this.state=/[a-zA-Z0-9\s]{3,}/.test(state);
+        this.zip=/^[0-9]{6}$/.test(zip);
+        this.phoneNumber=/[5-9]{1}[0-9]{9}/.test(phoneNumber);
+        this.email=/[a-z]{1,}([-+_])?([.][a-zA-Z0-9]{2,})?([A-Za-z0-9]{1,})?@[a-z0-9]{1,}.[a-z]{2,3}(.[a-z]{2,3})?/.test(email);
     }
 }
-let nagrajPanchal = new Contacts('Nagraj','Panchal','Shivanagar','Aurad','Karnataka','585326','9739713842','nagraj.panchal@gmail.com');
+
+function checkName(firstName)
+{
+    /[A-Z]{1}[a-z]{2,}/.test(firstName);
+}
+
+
+var nagrajPanchal = new Contacts('Nagraj','Panchal','Shivanagar','Aurad','Karnataka','585326','9739713842','NagrajPanchal@gmail.com');
 console.log(nagrajPanchal);
+// console.log(checkName("Nagraj"));
+
